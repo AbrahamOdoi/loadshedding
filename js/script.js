7f40e874d37f03d50f9c638d63b46d3c1018a7f6
AbrahamOdoi <abrahamodoi2002@gmail.com>
2014-03-21 15:23:13 +0000

    1.3
    1.3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
 $(document).ready(function() {
-	// document.addEventListener("deviceready", onDeviceReady, false);
-	// onDeviceReady();
+	document.addEventListener("deviceready", onDeviceReady, false);
 
-	// function onDeviceReady() {
-	$('#bulb').fadeOut(3000, function() {
+	function onDeviceReady() {
+		var db = window.openDatabase("dbsched", "1.0", "db_sched", 200000);
+		db.transaction(populateDB, errorCB, successCB);
+	}
+
+	// Populate the database
+	//
+	function populateDB(tx) {
+		tx.executeSql('DROP TABLE IF EXISTS tbl_sched');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS tbl_sched (id unique, data)');
+		tx.executeSql('INSERT INTO tbl_sched (id, data) VALUES (1, "First row")');
+		tx.executeSql('INSERT INTO tbl_sched (id, data) VALUES (2, "Second row")');
+	}
+
+	// Transaction error callback
+	//
+	function errorCB(err) {
+		alert("Error processing SQL: " + err);
+	}
+
+	// Transaction success callback
+	//
+	function successCB() {
+		alert("success!");
+	}
+
+
+	$('#bulb').fadeOut(4000, function() {
 		$('#page').css('background', 'black');
 		$('#candle').fadeIn(4000, function() {
 			window.location = 'mainpage.php';
